00b93c6c17db79fd1721ff87873eb3eb50df0107
Prajwal S <prajwalplays@gmail.com>
2024-04-12 22:42:51 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,10 +4,10 @@ type Props = {};
 
 export default function Navbar({}: Props) {
   return (
-    <header className='p-4 text-white bg-navBg sticky z-100'>
+    <header className='p-4 text-white bg-navBg sticky z-10 bg-transparent'>
       <div className='font-Montserrat font-semibold max-w-[1300px] flex mx-auto items-center sticky'>
         <Link to='/'>
-          <h1 className=''> Weather App</h1>
+          <h1 className='mix-blend-overlay'> Weather App</h1>
         </Link>
       </div>
     </header>
