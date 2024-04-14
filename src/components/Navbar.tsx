c1956ca343e1a51d9a4fd7376f1708cd182f68e2
Prajwal S <prajwalplays@gmail.com>
2024-04-15 00:00:56 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
 
-type Props = {};
+type Props = {
+  backgroundColor : string;
+};
 
-export default function Navbar({}: Props) {
+export default function Navbar({backgroundColor}: Props) {
   return (
-    <header className='p-4 text-white bg-navBg sticky z-10 bg-transparent'>
+    <header className={`p-4 text-white sticky z-10 ${backgroundColor}`}>
       <div className='font-Montserrat font-semibold max-w-[1300px] flex mx-auto items-center sticky'>
         <Link to='/'>
           <h1 className='mix-blend-overlay'> Weather App</h1>
