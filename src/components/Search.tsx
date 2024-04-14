03a0f12f38d757a149acb86b9aad8dd1d7fbcf8a
Prajwal S <prajwalplays@gmail.com>
2024-04-15 00:01:01 +0530

    Update Search.tsx

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -7,7 +7,7 @@ type Props = {
 export default function Search({onSearchChange}: Props) {
   return (
     <div className='items-center flex justify-center mx-auto p-3'>
-        <input type="search" placeholder='search city' className='outline outline-1 p-2 rounded-lg focus:border-0 focus:ring-0 active:border-0 active:ring-0 active:bg-none' onChange={onSearchChange} />
+        <input type="search" placeholder='search city' className='outline outline-1 p-2 border-none outline-none opacity-95 focus:border-0 focus:ring-0 active:border-0 active:ring-0 active:bg-none text-black' onChange={onSearchChange} />
     </div>
   )
 }
