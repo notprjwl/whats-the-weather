164e4f90a4f257ca9a42444a1bf197ebfba410cd
Prajwal S <prajwalplays@gmail.com>
2024-04-15 00:32:51 +0530

    Update CtoF.tsx

--- src/components/CtoF.tsx
+++ src/components/CtoF.tsx
@@ -9,7 +9,7 @@ const CtoF = ({onToggleTemperature, isCelsius}: Props) => {
   return (
     <div className='flex'>
         <button className='bg-[#707c8188] sm:text-xs bg-opacity-100 p-[] px-2 rounded-lg hover:bg-opacity-50 transition-all ease-in-out duration-500' onClick={onToggleTemperature}>
-        {isCelsius ? 'Fahrenheit' : 'Celsius'}
+        {isCelsius ? 'Celsius' : 'Fahrenheit'}
         </button>
     </div>
   )
