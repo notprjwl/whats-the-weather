57b60153d501d783aa398262fbbf4e4f0552df2e
Prajwal S <prajwalplays@gmail.com>
2024-04-15 14:47:41 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,19 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 type Props = {
-  backgroundColor : string;
+  backgroundColor: string;
+  handleHover: () => void;
+  currentColor : string;
 };
 
-export default function Navbar({backgroundColor}: Props) {
+export default function Navbar({ backgroundColor, handleHover, currentColor }: Props) {
+
   return (
-    <header className={`p-4 text-white sticky z-10 ${backgroundColor}`}>
+    <header className={`p-4 text-white sticky top-0 z-10 ${backgroundColor}`}>
       <div className='font-Montserrat font-semibold max-w-[1300px] flex mx-auto items-center sticky'>
         <Link to='/'>
-          <h1 className='mix-blend-overlay'> Weather App</h1>
+          <h1 className={`mix-blend-overlay leading-4`} onMouseEnter={handleHover}> what's the <br /> <span className={`italic ${currentColor} transition-all duration-500 ease-in-out` }>weather?</span> </h1>
         </Link>
       </div>
     </header>
