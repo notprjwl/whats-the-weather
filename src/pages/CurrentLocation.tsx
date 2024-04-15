56aa682e45f74cce689912fd8dd1b110e3b25a92
Prajwal S <prajwalplays@gmail.com>
2024-04-15 14:58:43 +0530

    Update CurrentLocation.tsx

--- src/pages/CurrentLocation.tsx
+++ src/pages/CurrentLocation.tsx
@@ -56,7 +56,7 @@ export default function CurrentLocation({}: Props) {
       }
     };
     fetchData();
-  }, [name]);
+  }, [latitude, longitude]);
 
   useEffect(() => {
     const interval = setInterval(() => {
