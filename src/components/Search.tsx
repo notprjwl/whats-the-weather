750911f58aa1a97a7a1b9a516e1c1e0cecc13230
Prajwal S <prajwalplays@gmail.com>
2024-04-15 17:15:18 +0530

    Update Search.tsx

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -7,9 +7,9 @@ type Props = {
 export default function Search({ onSearchChange }: Props) {
   return (
     <>
-      <h1 className="text-center">OR <br /> Search any city from the table</h1>
+      {/* <h1 className="text-center">OR <br /> Search any city from the table</h1> */}
       <div className='items-center flex justify-center mx-auto p-3'>
-        <input type='search' placeholder='search your city' className='outline outline-1 p-2 border-none outline-none opacity-95 focus:border-0 focus:ring-0 active:border-0 active:ring-0 active:bg-none text-black' onChange={onSearchChange} />
+        <input type='search' placeholder='search your city' className='outline outline-1 p-2 border-none outline-none opacity-95 focus:border-0 focus:ring-0 active:border-0 active:ring-0 active:bg-none text-black rounded-md' onChange={onSearchChange} />
       </div>
     </>
   );
