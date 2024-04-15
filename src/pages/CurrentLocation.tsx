95e340ecbbd74e51f7581181a865ca15dcf168b9
Prajwal S <prajwalplays@gmail.com>
2024-04-15 18:18:29 +0530

    Update CurrentLocation.tsx

--- src/pages/CurrentLocation.tsx
+++ src/pages/CurrentLocation.tsx
@@ -85,77 +85,77 @@ export default function CurrentLocation({}: Props) {
 
   return (
     <>
-    <Navbar
-      backgroundColor='bg-transparent'
-      handleHover={function (): void {
-        throw new Error("Function not implemented.");
-      }}
-      currentColor={""}
-    />
-    {loading ? (
-      <>
-        <Loading />
-      </>
-    ) : (
-      <>
-        {weatherDescription && <WeatherVideo weatherDescription={weatherDescription} dayOrNight={dayOrNight} />}
-        {error && <div className='text-red-500'>{error}</div>}
-        <div className=''>
-          <div className={`w-full mx-auto flex justify-center items-center absolute inset-0 transition-all duration-500 ease-in-out transform`}>
-            <div className={`rounded-2xl text-white bg-opacity-30 bg-black h-[50vh] w-[26rem] sm:w-[20rem] p-5 z-10 m-5 transition-all duration-500 ease-in-out`}>
-              <div className='text-gray-200 font-mono flex justify-between'>
-                <h1 className='text-1xl sm:text-xs transition-all ease-in-out duration-500'> Current Weather </h1>
-                <h1 className='tracking-tighter text-[1rem] sm:text-xs transition-all ease-in-out duration-500'> {date.toLocaleString()} </h1>
-              </div>
-              <div className='flex justify-between'>
-                <h1 className='text-sm font-light italic'>{forecastLive?.weather[0].description}</h1>
-                <CtoF onToggleTemperature={handleToggleTemperature} isCelsius={isCelsius} />
-              </div>
-              <div className='inline p-3 font-outfit transition-all ease-in-out duration-500'>
-                <div className='flex gap-2 justify-between'>
-                  <h1 className='text-7xl sm:text-3xl transition-all ease-in-out duration-500 text-clamp font-semibold '>{forecast?.name}</h1>
-                  <div className='text-1xl h-full'>
-                    <div className='flex gap-2 justify-between'>
-                      <h1 className='flex'>
-                        <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={3} stroke='currentColor' className='w-3 h-6'>
-                          <path strokeLinecap='round' strokeLinejoin='round' d='M4.5 10.5 12 3m0 0 7.5 7.5M12 3v18' />
-                        </svg>
-                        {forecastLive?.main.temp_max && (isCelsius ? KtoC(forecastLive?.main.temp_max) : KtoF(forecastLive?.main.temp_max))}°<span className='text-1xl opacity-90'>{isCelsius ? "C" : "F"}</span>
-                      </h1>
-                      <h1 className='flex '>
-                        <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={3} stroke='currentColor' className='w-3 h-6'>
-                          <path strokeLinecap='round' strokeLinejoin='round' d='M19.5 13.5 12 21m0 0-7.5-7.5M12 21V3' />
-                        </svg>
-                        {forecastLive?.main.temp_min && (isCelsius ? KtoC(forecastLive.main.temp_min) : KtoF(forecastLive.main.temp_min))}°<span className='text-1xl opacity-90'>{isCelsius ? "C" : "F"}</span>
-                      </h1>
-                    </div>
-                    <div className='flex gap-1 justify-between text-sm '>
-                      pop: {forecast?.list[0].pop}%<h1></h1>
-                      <h1 className='flex'>
-                        <svg className='w-4 h-5' aria-hidden='true' xmlns='http://www.w3.org/2000/svg' width='24' height='24' fill='currentColor' viewBox='0 0 24 24'>
-                          <path d='M11.782 5.72a4.773 4.773 0 0 0-4.8 4.173 3.43 3.43 0 0 1 2.741-1.687c1.689 0 2.974 1.972 3.758 2.587a5.733 5.733 0 0 0 5.382.935c2-.638 2.934-2.865 3.137-3.921-.969 1.379-2.44 2.207-4.259 1.231-1.253-.673-2.19-3.438-5.959-3.318ZM6.8 11.979A4.772 4.772 0 0 0 2 16.151a3.431 3.431 0 0 1 2.745-1.687c1.689 0 2.974 1.972 3.758 2.587a5.733 5.733 0 0 0 5.382.935c2-.638 2.933-2.865 3.137-3.921-.97 1.379-2.44 2.208-4.259 1.231-1.253-.673-2.19-3.443-5.963-3.317Z' />
-                        </svg>
-                        {forecastLive?.wind.speed}
-                      </h1>
-                    </div>
-                  </div>
+      <Navbar
+        backgroundColor='bg-transparent'
+        handleHover={function (): void {
+          throw new Error("Function not implemented.");
+        }}
+        currentColor={""}
+      />
+      {loading ? (
+        <>
+          <Loading />
+        </>
+      ) : (
+        <>
+          {weatherDescription && <WeatherVideo weatherDescription={weatherDescription} dayOrNight={dayOrNight} />}
+          {error && <div className='text-red-500'>{error}</div>}
+          <div className=''>
+            <div className={`w-full mx-auto flex justify-center items-center absolute inset-0 transition-all duration-500 ease-in-out transform`}>
+              <div className={`rounded-2xl text-white bg-opacity-30 bg-black h-[50vh] w-[26rem] sm:w-[20rem] p-5 z-10 m-5 transition-all duration-500 ease-in-out`}>
+                <div className='text-gray-200 font-mono flex justify-between'>
+                  <h1 className='text-1xl sm:text-xs transition-all ease-in-out duration-500'> Current Weather </h1>
+                  <h1 className='tracking-tighter text-[1rem] sm:text-xs transition-all ease-in-out duration-500'> {date.toLocaleString()} </h1>
                 </div>
-
-                <div className='flex'>
-                  <div className='font-semibold inline pt-1 transition-all ease-in-out duration-500'>
-                    {temperature !== null && (
-                      <div className='inline text-8xl sm:2xl '>
-                        {isCelsius ? KtoC(temperature) : KtoF(temperature)}°<span className='text-4xl opacity-90'>{isCelsius ? "C" : "F"}</span>
+                <div className='flex justify-between'>
+                  <h1 className='text-sm font-light italic'>{forecastLive?.weather[0].description}</h1>
+                  <CtoF onToggleTemperature={handleToggleTemperature} isCelsius={isCelsius} />
+                </div>
+                <div className='inline p-3 font-outfit transition-all ease-in-out duration-500'>
+                  <div className='flex gap-2 justify-between'>
+                    <h1 className='text-7xl sm:text-3xl transition-all ease-in-out duration-500 text-clamp font-semibold '>{forecast?.name}</h1>
+                    <div className='text-1xl h-full'>
+                      <div className='flex gap-2 justify-between'>
+                        <h1 className='flex'>
+                          <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={3} stroke='currentColor' className='w-3 h-6'>
+                            <path strokeLinecap='round' strokeLinejoin='round' d='M4.5 10.5 12 3m0 0 7.5 7.5M12 3v18' />
+                          </svg>
+                          {forecastLive?.main.temp_max && (isCelsius ? KtoC(forecastLive?.main.temp_max) : KtoF(forecastLive?.main.temp_max))}°<span className='text-1xl opacity-90'>{isCelsius ? "C" : "F"}</span>
+                        </h1>
+                        <h1 className='flex '>
+                          <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={3} stroke='currentColor' className='w-3 h-6'>
+                            <path strokeLinecap='round' strokeLinejoin='round' d='M19.5 13.5 12 21m0 0-7.5-7.5M12 21V3' />
+                          </svg>
+                          {forecastLive?.main.temp_min && (isCelsius ? KtoC(forecastLive.main.temp_min) : KtoF(forecastLive.main.temp_min))}°<span className='text-1xl opacity-90'>{isCelsius ? "C" : "F"}</span>
+                        </h1>
                       </div>
-                    )}
-                    <h1 className='text-1xl font-light'>
-                      Real Feel {forecastLive && (isCelsius ? KtoC(forecastLive.main.feels_like) : KtoF(forecastLive.main.feels_like))}°<span className='text-xs'>{isCelsius ? "C" : "F"}</span>
-                    </h1>
+                      <div className='flex gap-1 justify-between text-sm '>
+                        pop: {forecast?.list[0].pop}%<h1></h1>
+                        <h1 className='flex'>
+                          <svg className='w-4 h-5' aria-hidden='true' xmlns='http://www.w3.org/2000/svg' width='24' height='24' fill='currentColor' viewBox='0 0 24 24'>
+                            <path d='M11.782 5.72a4.773 4.773 0 0 0-4.8 4.173 3.43 3.43 0 0 1 2.741-1.687c1.689 0 2.974 1.972 3.758 2.587a5.733 5.733 0 0 0 5.382.935c2-.638 2.934-2.865 3.137-3.921-.969 1.379-2.44 2.207-4.259 1.231-1.253-.673-2.19-3.438-5.959-3.318ZM6.8 11.979A4.772 4.772 0 0 0 2 16.151a3.431 3.431 0 0 1 2.745-1.687c1.689 0 2.974 1.972 3.758 2.587a5.733 5.733 0 0 0 5.382.935c2-.638 2.933-2.865 3.137-3.921-.97 1.379-2.44 2.208-4.259 1.231-1.253-.673-2.19-3.443-5.963-3.317Z' />
+                          </svg>
+                          {forecastLive?.wind.speed}
+                        </h1>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className='flex'>
+                    <div className='font-semibold inline pt-1 transition-all ease-in-out duration-500'>
+                      {temperature !== null && (
+                        <div className='inline text-8xl sm:2xl '>
+                          {isCelsius ? KtoC(temperature) : KtoF(temperature)}°<span className='text-4xl opacity-90'>{isCelsius ? "C" : "F"}</span>
+                        </div>
+                      )}
+                      <h1 className='text-1xl font-light'>
+                        Real Feel {forecastLive && (isCelsius ? KtoC(forecastLive.main.feels_like) : KtoF(forecastLive.main.feels_like))}°<span className='text-xs'>{isCelsius ? "C" : "F"}</span>
+                      </h1>
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div>
-               <section className='flex overflow-x-scroll justify-start p-2 my-2'>
+                <div>
+                  <section className='flex overflow-x-scroll justify-start p-2 my-2'>
                     {forecast?.list.map((item, i) => (
                       <div className='inline-block text-center px-3' key={i}>
                         <p className="font-outfit">{i === 0 ? "Now" : new Date(item.dt * 1000 + 5.5 * 60 * 60 * 1000).getUTCHours()}</p>
@@ -168,12 +168,12 @@ export default function CurrentLocation({}: Props) {
                       </div>
                     ))}
                   </section>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </>
-    )}
-  </>
+        </>
+      )}
+    </>
   );
 }
